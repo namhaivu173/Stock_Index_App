2f48dd1f2d0d45488b1d98ea18e5286903e794f1
Hai Vu <namhaivu97@gmail.com>
2023-04-12 17:00:32 -0400

    Update indices-app.py

--- indices-app.py
+++ indices-app.py
@@ -982,7 +982,7 @@ with c2:
     pred_price3 = pred_price2[['Close','Predictions']].tail(10)
     pred_price3 = pred_price3.style.highlight_null(props="color: transparent;") # hide NAs
     st.write("#### Actual vs. Predicted Prices (last 5 & next 5 trading days)")
-    st.dataframe(pred_price3)
+    st.dataframe(pred_price3, width=c2.width)
     st.markdown(filedownload(pred_price2), unsafe_allow_html=True)
 
 c1, c2 = st.columns(2)
