9c6143146c4d891f6d4fce173848c3905b44250f
Hai Vu <namhaivu97@gmail.com>
2023-04-12 16:25:12 -0400

    Update indices-app.py

--- indices-app.py
+++ indices-app.py
@@ -957,7 +957,6 @@ pred_new['Predictions'] = new_price
 
 # Set the values in the 'Closing Price' column to NaN for the last 5 rows
 pred_price2 = pd.concat([pred_price, pred_new], axis=0)
-pred_price2 = pred_price2.style.highlight_null(props="color: transparent;") # hide NAs
 
 #########################################################
 
@@ -984,6 +983,7 @@ with c2:
     st.table(pred_price2[['Close','Predictions']].tail(10))
     st.markdown(filedownload(pred_price2), unsafe_allow_html=True)
 
+pred_price2 = pred_price2.style.highlight_null(props="color: transparent;") # hide NAs
 c1, c2 = st.columns(2)
 with c1:
     st.write("#### Actual vs. Predicted price chart")
