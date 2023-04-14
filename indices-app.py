a0b12de5ef27be8553812a9e87f0f47a251b747a
Hai Vu <namhaivu97@gmail.com>
2023-04-14 12:48:34 -0400

    Update indices-app.py

--- indices-app.py
+++ indices-app.py
@@ -1023,7 +1023,7 @@ with tab4:
 	#########################################################
 	st.write('### Prediction Model Outputs')
 	
-	st.write(f"Today's Date is: <b>{datetime.utcnow().strftime('%Y/%m/%d')} UTC</b>", unsafe_allow_html=True)
+	st.write(f"Today's Date is: <b>{pd.to_datetime(current_date, utc=True).strftime('%Y/%m/%d')} UTC</b>", unsafe_allow_html=True)
 	
 	# Pick one ticker to predict
 	#pick_ticker = '^GSPC'
