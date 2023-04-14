9ea565f55bc0f68a42da7045e55bde3b871fb493
Hai Vu <namhaivu97@gmail.com>
2023-04-14 16:27:12 -0400

    Update indices-app.py

--- indices-app.py
+++ indices-app.py
@@ -868,11 +868,10 @@ with tab3:
 
 			if st.form_submit_button(label='Calculate VaR'):
         			if initial_inv >= 1 and initial_inv <= 10000000 and periods >= 1 and periods <= 252 and conf_level >= 0.5 and conf_level <= 0.999: 
-            				# run simulation
             				pass
        				else:
             				st.error("Invalid input values. Please check your inputs and try again.")
-					st.stop()
+					#st.stop()
 					
 		c1, c2 = st.columns(2)
 		
