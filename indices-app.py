96ad5a96da510de5b7c1147ee280c49749796f42
Hai Vu <namhaivu97@gmail.com>
2025-09-30 13:04:14 -0700

    Update indices-app.py

--- indices-app.py
+++ indices-app.py
@@ -23,6 +23,7 @@ import plotly.figure_factory as ff
 import streamlit as st
 import scipy
 import lxml
+import html5lib
 from scipy.stats import norm
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.neural_network import MLPRegressor
